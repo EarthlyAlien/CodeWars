8e1a2f2fc628ffe7cc3b0a07035ac9fdb6af1b29
Chaitanya Vankadaru <chaitanya.vankadaru@gmail.com>
2019-03-15 11:45:50 +0530

    Update youareasquare.py

--- youareasquare.py
+++ youareasquare.py
@@ -8,3 +8,17 @@ def is_square(n):
         return True
     else:
         return False
+
+# Alternate recommended solutions
+# • No.1 :
+# import math
+# def is_square(n):
+#     if n < 0:
+#         return False
+#     sqrt = math.sqrt(n)    
+#     return sqrt.is_integer()
+
+# • No.2 :
+# import math
+# def is_square(n):
+#     return n > -1 and math.sqrt(n) % 1 == 0;
